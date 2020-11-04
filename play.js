b9d651266e370ebabd4e794ef10bf6e0c133b2aa
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-11-04 23:16:54 +0000

    server setup for game

---
+++ play.js
@@ -0,0 +1,20 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '135.23.222.131',
+    port: 50542
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+  conn.on('data', data => {
+    console.log(data);
+  });
+
+  conn.on('data', (port) => {
+    console.log('Message from client: ', port)
+  });
+  return conn;
+}
+console.log('Connecting ...');
+connect();
